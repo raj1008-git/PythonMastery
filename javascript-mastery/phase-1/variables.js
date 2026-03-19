5dece8a1258e215d9c52ee2e0039b95c2503fe69
raj1008-git <macof.rj@gmail.com>
2026-03-19 17:56:23 +0545

    Learned the var,let and const

---
+++ javascript-mastery/phase-1/variables.js
@@ -0,0 +1,11 @@
+const username = "Raj";
+let loginCount = 0;
+loginCount = loginCount + 1;
+console.log(username);
+console.log(loginCount);
+
+const appName = "Mindset";
+let activeUsers = 0;
+activeUsers = activeUsers + 5;
+console.log(appName);
+console.log(activeUsers);
